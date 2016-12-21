8e7a0dc9c192e1ea61e3662d1da62121ddc8702a
sklegg <sklegg@gmail.com>
2016-12-20 19:14:26 -0800

    initial commit

---
+++ src/main/kotlin/sklegg-github/gameobjects/AbstractShip.kt
@@ -0,0 +1,36 @@
+package `sklegg-github`.gameobjects
+
+/**
+ * Created by scott on 12/20/16.
+ * Abstract Ship
+ */
+abstract class AbstractShip constructor() {
+
+    abstract val shipType: String
+    abstract val purchaseCost: Int
+    /*abstract val maxAtomicDetinators: Int
+    abstract val maxMarkerBeacons: Int
+    abstract val maxCombrite: Int
+    abstract val maxCloaking: Int
+    abstract val maxEtherProbes: Int
+    abstract val planetScanner: Boolean
+    abstract val turnsPerWarp: Int
+    abstract val maxLimpitMines: Int
+    abstract val maxArmidMines: Int
+    abstract val maxPhotonMissiles: Int
+
+    abstract val longRangeScanner: String
+    abstract val maxMineDisrupters: Int
+    abstract val maxGenesisTorpedoes: Int
+    abstract val offensiveShipOdds: Int
+    abstract val defensiveShipOdds: Int
+    abstract val transwarpDrive: Boolean
+    abstract val psychicProbe: Boolean
+    abstract val maxHolds: Int
+    abstract val maxFighters: Int
+    abstract val maxShields: Int
+    abstract val maxFightersPerAttack: Int
+    abstract val transportRange: Int
+    abstract val shipValue: String
+*/
+}
